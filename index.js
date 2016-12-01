1c81ad83a4203e653bf426f5874f9c146f4f5351
Malinna Leach <malinnab@yahoo.com>
2016-12-01 16:43:12 +0000

    adjusting port settings

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const express = require('express'),
 const router = require('./router');
 var models = require('./models');
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 router(app);
 app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
 
 
 models.sequelize.sync().then(function() {
